1fa9302f1de1eff96d6d275ff098e3ac7f575508
Le Nghia <ltnghia1234@gmail.com>
2023-11-13 10:47:57 +0700

    state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,24 @@
 import React from 'react';
 
 export class MyComponent extends React.Component {
+    state = {
+        name: "nghĩa",
+        age: 12
+    }
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
+
         return (
-            <div>hello my component nghia</div>
+            <React.Fragment>
+                <input type='text' value={this.state.name} onChange={(event) => { this.handleOnChange(event) }} />
+                <div>hello my component {this.state.name}</div>
+                <div>tuổi : {this.state.age}</div>
+            </React.Fragment>
         )
 
     }
